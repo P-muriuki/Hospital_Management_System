b1c8dbb59d48ba503430ae7a6791a87e64e9f48f
Petra <wairimumuriuki07@gmail.com>
2024-02-09 10:35:54 +0300

    Start with the project first

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\petra\Projects folder\Hospital_Management_System\.venv
+set VIRTUAL_ENV=C:\Users\petra\ProjectsFolder\Hospital_Management_System\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- HospitalSystemProjectFolder/HospitalStorage/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HospitalstorageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'HospitalStorage'
